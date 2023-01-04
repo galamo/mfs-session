614b60dad2ea19759f46784d4bf1e6dcf4570965
Gal Amouyal <galamouy@gals-mbp.trusteer.il.ibm.com>
2023-01-04 13:06:51 +0200

    changes

--- mfs-clients/container/src/App.tsx
+++ mfs-clients/container/src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+// @ts-ignore
+// import CartPage from "Cart/CartPage";
+// @ts-ignore
+// import ProductsPage from "Products/ProductsPage"
 import "./index.css";
 
 const App = () => (
   <div className="container">
-    <h1> Container -   Host  </h1>
+    <h1 style={{ fontSize: "3rem" }}> Container -   Host  </h1>
     <div className="host">
-      <div className="mfItem">
-        Loading Products...
+      <div className="mfItem" style={{ overflow: "auto", maxHeight: "600px" }}>
+        <h1>Products</h1>
+        <div className="content">
+          {/* <ProductsPage /> */}
+        </div>
       </div>
       <div className="mfItem">
-        Loading Cart...
+        <h1> Your Cart </h1>
+        <div className="content">
+          {/* <CartPage /> */}
+        </div>
       </div>
     </div>
   </div>
